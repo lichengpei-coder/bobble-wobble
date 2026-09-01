f9a6172293abe788386b6da38479bad9ceb0e8e9
yu tung <yutung.lau8@gmail.com>
2026-09-01 09:20:51 +0800

    Change base model from EfficientNet B7 to B0

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import timm
 class AIImageDetector(nn.Module):
     def __init__(self, num_classes=53):
         super(AIImageDetector, self).__init__()
-        self.base_model = timm.create_model('efficientnet_b7', pretrained=True)
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
         self.features = nn.Sequential(*list(self.base_model.children())[:-1])
         enet_output_size = 1280
         # Making a classifier
